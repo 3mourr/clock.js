312fd5fcf5a012c0441a804389c8074f77a38785
Omar Reda <142605087+3mourr@users.noreply.github.com>
2025-08-19 15:34:25 +0300

    Delete index2.js

--- index2.js
+++
@@ -1,9 +0,0 @@
-const minHand = document.querySelector('.min-hand');
-
-function setTo7PM() {
-  const minutes = 0; // 0 minutes for 7:00
-  const degrees = (minutes / 60) * 360;
-  minHand.style.transform = `rotate(${degrees}deg)`;
-}
-
-setTo7PM();
